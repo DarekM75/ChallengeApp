7b14623f76d62dd8e5eed0d5441f572d1cfc45e6
DarekM75 <dariusz.malkowski@gmail.com>
2023-08-22 23:47:16 +0200

    Zadanie domowe dzień 4.

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Darek!");
+﻿using System;
+
+var name = "Ewa";
+var age = 33;
+var sex = 'f';
+//f - female, m - male; l - lgbt;
+
+if (sex == 'f')
+{
+    if (age == 33 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa lat 33.");
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat.");
+    }
+}
+else if (age < 18)
+    if (sex == 'm')
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna.");
+    }
+    else
+
+    {
+        Console.WriteLine("Dane poza zakresem.");
+    }
+
+
